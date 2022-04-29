4b8cc28eea2173ebd4dab406ff8d54ee12fd25d6
sopurdey <a_purdey@hotmail.com>
2022-04-29 17:28:24 +0200

    tests Article (UseArticleIdao)

--- src/main/java/org/diginamic/fr/jdbc/UseArticleIdao.java
+++ src/main/java/org/diginamic/fr/jdbc/UseArticleIdao.java
@@ -13,6 +13,9 @@ public class UseArticleIdao {
 		 */
 		try {
 			ArticleIdao arti = new ArticleIdao();
+			//addArticle(arti);
+			//updateArticle(arti);
+			//deleteArticle(arti);
 			getAffiche(arti);
 		} catch (Exception ex) {
 			System.err.println(ex.getMessage());
@@ -27,4 +30,34 @@ public class UseArticleIdao {
 				.println(art.getRef() + " " + art.getDesignation() + " " + art.getPrix() + "€ "));
 	}
 
+
+	public static void addArticle(IDao<Article> o) {
+		Article artTest = new Article(11, "F03", "Boulon laiton 6 x 12 mm (sachet de 8)", 3.20, 3);
+		try {			
+			o.insert(artTest);
+		} catch(Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+		getAffiche(o);
+	}
+	
+	public static void updateArticle(IDao<Article> o) {
+		Article oldA = new Article(11, "F03", "Boulon laiton 6 x 12 mm (sachet de 8)", 3.20, 3);
+		Article newA = new Article(11, "F03", "Boulon laiton 6 x 12 mm (sachet de 10)", 3.85, 3);
+		try {
+			o.update(oldA, newA);
+		} catch(Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+		getAffiche(o); 
+	}
+	
+	public static void deleteArticle(IDao<Article> o) {
+		Article testDel = new Article(11, "F03", "Boulon laiton 6 x 12 mm (sachet de 10)", 3.85, 3);
+		try {
+			o.delete(testDel);
+		} catch(Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
 }
